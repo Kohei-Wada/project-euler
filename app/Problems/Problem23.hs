1c66c42e20dfa6d6a41b3297b27a7eb6f33c0e97
Kohei Wada <program3152019@gmail.com>
2022-06-02 14:49:17 +0900

    problem23

--- app/Problems/Problem23.hs
+++ app/Problems/Problem23.hs
@@ -2,13 +2,31 @@
 
 module Problems.Problem23 where
 
+import Data.List
+import Data.List.Unique
 import Factors 
 
 
+isPerfectNum :: Integer -> Bool
 isPerfectNum n = 2 * n == (sum $ factors n)
 
 
+isAbundantNum :: Integer -> Bool
+isAbundantNum n = 2 * n < (sum $ factors n)
+
+
+abundantNums :: Integer -> [Integer]
+abundantNums n = filter isAbundantNum [1..n]
+
+
+--todo
 problem23 :: IO ()
 problem23 = do 
-    print $ filter isPerfectNum [1..10000]
+    let abndnts = filter isAbundantNum [12..28123]
+        abnabn  = uniq $ sort [x + y | x <- abndnts, y <- abndnts, x + y < 28123]
+        target  = filter (\x -> not $ elem x abnabn) [1..28123] 
+
+    print $ sum target
+
+
 
