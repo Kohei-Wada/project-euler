9ce415c01052c40416efb8ee4073046cb6f3524c
Kohei Wada <program3152019@gmail.com>
2022-06-07 00:47:20 +0900

    problem35

--- app/Problems/Problem35.hs
+++ app/Problems/Problem35.hs
@@ -4,12 +4,29 @@ import Eratosthenes
 import Factors 
 
 
+rotate :: [a] -> [a]
+rotate (x:xs) = xs ++ [x]
+
+
 cycleNum :: Int -> Int 
-cycleNum n = let (x:xs) = show n in read (xs ++ [x]) :: Int
+cycleNum n = read $ rotate $ show n 
 
 
+cycleList :: Int -> [Int]
+cycleList n = tmp (n, (length . show) n) 
+    where tmp (n, l) = 
+            if l /= 0 
+               then let next = cycleNum n in [next] ++ tmp (next, l - 1) 
+               else []
+
+
+isCyclePrime :: Int -> Bool
+isCyclePrime n = 
+    if elem '0' (show n) 
+       then False
+       else foldl (\b n -> b && (isPrime . fromIntegral) n) True (cycleList n)
 
 
 problem35 :: IO () 
 problem35 = do 
-    print $ cycleNum 197
+    print $ filter isCyclePrime $ erat [2..1000000]
