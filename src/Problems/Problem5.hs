90e1c93a4193770c340a191c9e2e6e787e4a86ed
Kohei Wada <program3152019@gmail.com>
2023-02-03 06:04:38 +0900

    Problem5

--- src/Problems/Problem5.hs
+++ src/Problems/Problem5.hs
@@ -1,9 +1,8 @@
 module Problems.Problem5 where
 
-
-lcmList :: (Integral a) => [a] -> a
-lcmList l = foldl (lcm) 1 l
-
+import Data.List (foldl')
 
 problem5 :: IO () 
-problem5 = print $ lcmList [1..100000]
+problem5 = do  
+    let ans = foldl' (lcm) 1 [1 .. 20]
+    print ans
