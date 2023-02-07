f33e97e13c0fc73c784262179841a4c947149067
Kohei Wada <program3152019@gmail.com>
2023-02-07 12:19:08 +0900

    Problem12

--- src/Problems/Problem12.hs
+++ src/Problems/Problem12.hs
@@ -1,16 +1,17 @@
 module Problems.Problem12 where
 
-import Factors
+import Data.List (group)
+import Factors (primeFactors) 
 
 
+triangulars :: [Int]
+triangulars = [n * (n - 1) `div` 2 | n <- [2..] ]
 
-triangular n = n * (n - 1) `div` 2
-
-triangulars = [triangular n | n <- [1..] ]
-
+countFactors :: Int -> Int
+countFactors n = product $ map (\xs -> length xs + 1) $ group $ primeFactors $ fromIntegral n
 
 problem12 :: IO () 
 problem12 = do
-    print $ length $ factors 76576500 
-    print $ head $ dropWhile(\x -> (length . factors) x < 500) triangulars
-    
+    let ans = head $ dropWhile (\x -> countFactors x < 500 ) triangulars
+    print ans
+
