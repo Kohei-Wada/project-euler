fb8f3ff34eed44e7199b24e91aef121facd791d6
Kohei Wada <program3152019@gmail.com>
2023-02-07 11:35:01 +0900

    Problem14

--- src/Problems/Problem14.hs
+++ src/Problems/Problem14.hs
@@ -1,19 +1,20 @@
-{-# LANGUAGE RecordWildCards #-}
-
 module Problems.Problem14 where
 
-import Control.Monad
-import Data.List
+collatzLength :: Int -> Int
+collatzLength n = go 1 n where
+    go l n 
+      | n == 1    = l 
+      | otherwise = if even n then go (l + 1) (div n 2) else go (l + 1) (3 * n + 1) 
 
-
-collatz :: Int -> [Int]
-collatz 1 = []
-collatz n = let a = if even n then n `div` 2 else 3 * n + 1 in a:collatz a
-
-
+findLongest :: Int -> Int
+findLongest n = go 1 1 1 where
+    go cur len ans
+      | cur == n  = ans
+      | otherwise = 
+          let len' = collatzLength cur  
+           in if len < len' then go (cur + 1) len' cur else go (cur + 1) len ans
 
 problem14 :: IO ()
 problem14 = do 
-    let list = map (length .collatz) [1..1000000]
-    print $ maximum list
---    print $ length $ collatz 837799
+    let ans = findLongest 1000000
+    print ans 
