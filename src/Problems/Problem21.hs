569b55cdbb2f10bedf70d5528c48dca23ae12941
Kohei Wada <program3152019@gmail.com>
2023-02-10 08:43:27 +0900

    Problem21

--- src/Problems/Problem21.hs
+++ src/Problems/Problem21.hs
@@ -1,16 +1,45 @@
+{-# LANGUAGE BangPatterns #-}
 module Problems.Problem21 where
 
-import Factors
+import Control.Monad (guard, forM_) 
+import Data.List (group) 
+
+d :: Int -> Int
+d 0 = 0 
+d n = go 1 0 where
+    go di acc
+      | di == n       = acc
+      | mod n di == 0 = go (di + 1) (acc + di) 
+      | otherwise     = go (di + 1) acc
 
 
-d :: Integer -> Integer
-d = sum . init . factors
+d' :: Int -> Int
+d' 0 = 0 
+d' n = go 1 n 2 2 0 - n 
+    where
+        go !acc !target !di !l !count 
+          | target == 1          = acc * geometoricSum di count
+          | target `mod` di == 0 = if l /= di then go (acc * geometoricSum l count) (target `div` di) di di 1 
+                                              else go acc (target `div` di) di di (count + 1) 
+          |  otherwise           = go acc target (di + 1) l count
+
+        geometoricSum _ 0 = 1 
+        geometoricSum a n = a ^ n + geometoricSum a (n - 1) 
 
 
-amicable :: Integer -> Integer 
-amicable n = let tmp = d n in if (d tmp) == n then tmp else 0
+amicables :: [Int]
+amicables = do 
+    n <- [1..]
+    let !tmp = d' n 
+    guard $ tmp /= n 
+    guard $ d' tmp == n 
+    pure n 
 
 
 problem21 :: IO () 
 problem21 = do 
-    print $ filter (\(x, a) -> a /= 0 && x /= a) $ map (\x -> (x, amicable x)) [220..100000]
+    let ans = sum $ takeWhile (< 10000) amicables
+    print ans
+
+
+
